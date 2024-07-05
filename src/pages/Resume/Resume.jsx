37c4f2c286cd8687763db72fcc00cdd77b83107a
tanvir244 <tr30365s25@gmail.com>
2024-07-05 23:14:17 +0600

    Resume Section completed fully

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,4 +1,13 @@
 import { Link } from "react-router-dom";
+// import html5 from './skills/html.png';
+// import css from './skills/css.png';
+// import tailwind from './skills/tailwind-css.png';
+// import js from './skills/js.png';
+// import react from './skills/react.png';
+// import firebase from './skills/firebase.png';
+// import mongodb from './skills/mongodb.png';
+// import node from './skills/node-jd.png';
+// import express from './skills/express.png';
 
 const Resume = () => {
     return (
@@ -25,13 +34,13 @@ const Resume = () => {
                 <p className='text-center mb-4 text-[#9f9f9f]'>Skills & Acedemic</p>
                 <h2 className='text-3xl md:text-5xl text-center font-bold text-white'>Resume Overview</h2>
                 <p className='w-24 h-1 bg-white mx-auto mt-8'></p>
-                <div className="flex justify-end gap-4">
+                <div className="flex justify-center md:justify-end gap-4 mt-4">
                     <a className="bg-[#1982c4] text-white hover:bg-white hover:text-black py-3 px-4 rounded-md font-semibold" href="https://docs.google.com/document/d/1Y630pTcynZo8He0bI5FLejhYnozvAVs_U0aa46QKyPs" target="_blank" rel="noopener noreferrer">View Resume</a>
                     <a className="bg-[#ffd60a] text-black hover:bg-white hover:text-black py-3 px-4 rounded-md font-semibold" href="https://drive.google.com/uc?export=download&id=19M9TgFXSFGl8otk8V36G7XER__YlKQN3">Download</a>
 
                 </div>
                 {/* Acedemic */}
-                <div className="my-12">
+                <div className="my-8">
                     <h2 className="text-4xl my-6 font-bold">Acedemic</h2>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                         <div className="card bg-neutral text-neutral-content">
@@ -62,8 +71,21 @@ const Resume = () => {
                 </div>
 
                 {/* Skills */}
-                <div>
-
+                <div className="space-y-4 py-20">
+                    <h2 className="text-4xl my-6 font-bold">Technical Skills</h2>
+                    <div className="flex justify-center gap-4 w-1/2 mx-auto">
+                        <img src="/public/skills/html.svg" className="w-[90px]" alt="" />
+                        <img src="/public/skills/css.svg" className="w-[90px]" alt="" />
+                        <img src="/public/skills/tailwind.svg" className="w-[90px]" alt="" />
+                        <img src="/public/skills/js.svg" className="w-[90px]" alt="" />
+                        <img src="/public/skills/react.svg" className="w-[90px]" alt="" />
+                    </div>
+                    <div className="flex justify-center gap-4 w-1/2 mx-auto">
+                        <img src="/public/skills/firebase.svg" className="w-[90px]" alt="" />
+                        <img src="/public/skills/node.svg" className="w-[90px]" alt="" />
+                        <img src="/public/skills/express.svg" className="w-[90px]" alt="" />
+                        <img src="/public/skills/mongodb.svg" className="w-[90px]" alt="" />
+                    </div>
                 </div>
             </div>
         </div>
